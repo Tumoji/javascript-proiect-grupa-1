6706ca321308df7c79d9e2f21d8c66af801156bf
Tumoji <133051676+Tumoji@users.noreply.github.com>
2023-12-10 02:09:34 +0000

    Deploying to gh-pages from @ Tumoji/javascript-proiect-grupa-1@f30a36aebbfb01298881f80f92715e88e882ce6d 🚀

---
+++ index.b2c3ab7c.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".loader-container");setTimeout((function(){document.querySelector(".loader-container").style.display="none"}),900),document.getElementById("search-form").addEventListener("submit",(function(t){t.preventDefault(),e.style.display="block",setTimeout((function(){e.style.display="none"}),900)}))}();
+//# sourceMappingURL=index.b2c3ab7c.js.map
---
+++ index.d4a93b6b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".loader-container");setTimeout((function(){document.querySelector(".loader-container").style.display="none"}),900);document.getElementById("search-form").addEventListener("submit",(function(t){t.preventDefault(),e.style.display="block",setTimeout((function(){e.style.display="none"}),900)}));
+//# sourceMappingURL=index.d4a93b6b.js.map
