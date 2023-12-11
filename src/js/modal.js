87fbdbc63ff6649acc12845e84d4182a22a71df6
adad22ro <gabirusu2000@gmail.com>
2023-12-11 23:09:42 +0200

    Creating the connection between the modal and the card

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,27 +1,38 @@
 // modal.js
 
-const modal = document.querySelector('.modal-movie-card');
-const overlay = document.querySelector('.overlay');
-const closeModalMovieCard = document.querySelector('.modal-movie-card-close');
-const openModalMovieCard = document.querySelector('.modal-movie-card-open');
+import axios from 'axios';
+import apiMovie from './api-movie.js';
+import { getMovieGenres } from './api-genres.js';
+import apiDetails from './api-details.js';
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
+export function openModal(movieId) {
+  const modal = document.querySelector('.modal-movie-card');
+  const overlay = document.querySelector('.overlay');
+  const closeModalMovieCard = document.querySelector('.modal-movie-card-close');
+  const openModalMovieCard = document.querySelector('.modal-movie-card-open');
 
-const openModal = function () {
+  const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  };
+
+  const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  };
+
+  closeModalMovieCard.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  });
+  // Setează conținutul modalului aici în funcție de movieId
+  const modalContent = document.querySelector('.modal-content');
+  modalContent.innerHTML = `<p>Afișezi detalii pentru filmul cu ID-ul: ${movieId}</p>`;
+
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
-};
-
-closeModalMovieCard.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
-
-export { openModal };
+}
