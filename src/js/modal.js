a35ebf5bcb20a8f18f001435802cb47d3e37c47f
adad22ro <gabirusu2000@gmail.com>
2023-12-16 14:24:53 +0200

    Functionalitati modal complete

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,8 +1,60 @@
-// modal.js
-
+import axios from 'axios';
+import apiMovie from './api-movie.js';
 import axios from 'axios';
 import apiMovie from './api-movie.js';
 
+// Declarațiile pentru butoane
+let addToQueueButton;
+let addToWatchedButton;
+
+// Funcția de adăugare a movie ID-ului în local storage
+function toggleLocalStorage(movieId, key) {
+  const storedIds = localStorage.getItem(key)
+    ? JSON.parse(localStorage.getItem(key))
+    : [];
+
+  // Verifică dacă ID-ul filmului este în storage
+  const index = storedIds.indexOf(movieId);
+  if (index !== -1) {
+    // Dacă este, îl elimină
+    storedIds.splice(index, 1);
+    console.log(`Movie ID ${movieId} removed from ${key}.`);
+  } else {
+    // Dacă nu este, îl adaugă
+    storedIds.push(movieId);
+    console.log(`Movie ID ${movieId} added to ${key}.`);
+  }
+
+  localStorage.setItem(key, JSON.stringify(storedIds));
+
+  // Actualizează textul butonului după modificare în stocarea locală
+  updateButtonText(
+    key === 'queue' ? addToQueueButton : addToWatchedButton,
+    movieId,
+    key
+  );
+}
+
+// Funcția de modificare a textului butonului
+function updateButtonText(button, movieId, key) {
+  const storedIds = localStorage.getItem(key)
+    ? JSON.parse(localStorage.getItem(key))
+    : [];
+
+  if (button) {
+    if (storedIds.includes(movieId)) {
+      button.textContent = `Remove from ${
+        key.charAt(0).toUpperCase() + key.slice(1)
+      }`;
+    } else {
+      button.textContent = `Add to ${
+        key.charAt(0).toUpperCase() + key.slice(1)
+      }`;
+    }
+  }
+}
+
+// Funcția de deschidere a modalului
 export async function openModal(movieId) {
   const modal = document.querySelector('.modal-movie-card');
   const overlay = document.querySelector('.overlay');
@@ -28,7 +80,7 @@ export async function openModal(movieId) {
     }
   });
 
-  // Fetch movie details from your API
+  // Importarea filmelor din API
   try {
     const response = await axios({
       method: apiMovie.method,
@@ -40,109 +92,63 @@ export async function openModal(movieId) {
 
     console.log(movieDetails);
 
-    // Set modal content using the retrieved movie details
+    // Adaugarea continutului modalului
     const modalContent = document.querySelector('.modal-content');
     modalContent.innerHTML = `
-    <div>
-    <img src = "https://image.tmdb.org/t/p/w500/${
-      movieDetails.poster_path
-    }" alt="${movieDetails.title}" class ="modal-movie-poster">
-    </div>
-    <div>
-      <p class="modal-movie-tille">Title: ${movieDetails.title}</p>
-      <p class="modal-movie-averageVote">Vote Average: ${
-        movieDetails.vote_average
-      }</p>
-      <p class="modal-movie-voteCount">Vote count: ${
-        movieDetails.vote_count
-      }</p>
-      <p class="modal-movie-popularity">Popularity: ${
-        movieDetails.popularity
-      }</p>
-      <p class="modal-movie-originalTitle">Original title: ${
-        movieDetails.original_title
-      }</p>
-      <p class="modal-movie-RealeseDate">Release Date: ${
-        movieDetails.release_date
-      }</p>
-      <p class="modal-movie-overview">Overview: ${movieDetails.overview}</p>
-      <p class="modal-movie-genders">Genres: ${movieDetails.genres
-        .map(genre => genre.name)
-        .join(', ')}</p>
-       <button class="add-queue-btn">add to queue</button>
-       <button class="add-watched-btn">add to Watched</button>
-    </div>
-
+      <div>
+        <img src="https://image.tmdb.org/t/p/w500/${
+          movieDetails.poster_path
+        }" alt="${movieDetails.title}" class="modal-movie-poster">
+      </div>
+      <div>
+        <p class="modal-movie-tille">Title: ${movieDetails.title}</p>
+        <p class="modal-movie-averageVote">Vote Average: ${
+          movieDetails.vote_average
+        }</p>
+        <p class="modal-movie-voteCount">Vote count: ${
+          movieDetails.vote_count
+        }</p>
+        <p class="modal-movie-popularity">Popularity: ${
+          movieDetails.popularity
+        }</p>
+        <p class="modal-movie-originalTitle">Original title: ${
+          movieDetails.original_title
+        }</p>
+        <p class="modal-movie-RealeseDate">Release Date: ${
+          movieDetails.release_date
+        }</p>
+        <p class="modal-movie-overview">Overview: ${movieDetails.overview}</p>
+        <p class="modal-movie-genders">Genres: ${movieDetails.genres
+          .map(genre => genre.name)
+          .join(', ')}</p>
+        <button class="add-queue-btn"></button>
+        <button class="add-watched-btn"></button>
+      </div>
     `;
 
-    // Function to toggle a movie ID in local storage
-    function toggleLocalStorage(movieId, key) {
-      const storedIds = localStorage.getItem(key)
-        ? JSON.parse(localStorage.getItem(key))
-        : [];
-
-      // Check if the movie ID is in the storage
-      const index = storedIds.indexOf(movieId);
-      if (index !== -1) {
-        // If the movie ID is found, remove it
-        storedIds.splice(index, 1);
-        console.log(`Movie ID ${movieId} removed from ${key}.`);
-      } else {
-        // If the movie ID is not found, add it
-        storedIds.push(movieId);
-        console.log(`Movie ID ${movieId} added to ${key}.`);
-      }
-
-      localStorage.setItem(key, JSON.stringify(storedIds));
-    }
-
-    // Function to update the button text based on the movie ID presence in local storage
-    function updateButtonText(button, movieId, key) {
-      const storedIds = localStorage.getItem(key)
-        ? JSON.parse(localStorage.getItem(key))
-        : [];
-
-      if (storedIds.includes(movieId)) {
-        button.textContent = `Remove from ${
-          key.charAt(0).toUpperCase() + key.slice(1)
-        }`;
-      } else {
-        button.textContent = `Add to ${
-          key.charAt(0).toUpperCase() + key.slice(1)
-        }`;
-      }
-    }
-
-    // Event listener for "Add to Queue" button
-    const addToQueueButton = document.querySelector('.add-queue-btn');
-    addToQueueButton.addEventListener('click', function () {
-      // Assuming you have a way to retrieve the current movie ID
-      const currentMovieId = movieId;
-
-      // Toggle the movie ID in the queue
-      toggleLocalStorage(currentMovieId, 'queue');
-
-      // Update the button text
-      updateButtonText(addToQueueButton, currentMovieId, 'queue');
-    });
-
-    // Event listener for "Add to Watched" button
-    const addToWatchedButton = document.querySelector('.add-watched-btn');
-    addToWatchedButton.addEventListener('click', function () {
-      // Assuming you have a way to retrieve the current movie ID
-      const currentMovieId = movieId;
-
-      // Toggle the movie ID in the watched list
-      toggleLocalStorage(currentMovieId, 'watched');
-
-      // Update the button text
-      updateButtonText(addToWatchedButton, currentMovieId, 'watched');
-    });
-
-    // Open the modal
+    // Deschiderea modalului
     openModal();
+
+    // Adaugarea evenimentelor de click după deschiderea modalului
+    addToQueueButton = document.querySelector('.add-queue-btn');
+    addToWatchedButton = document.querySelector('.add-watched-btn');
+
+    // Adaugarea evenimentelor de click pentru butoane
+    addToQueueButton.addEventListener('click', function () {
+      const currentMovieId = movieId;
+      toggleLocalStorage(currentMovieId, 'queue');
+    });
+
+    addToWatchedButton.addEventListener('click', function () {
+      const currentMovieId = movieId;
+      toggleLocalStorage(currentMovieId, 'watched');
+    });
+
+    // Actualizarea textului butoanelor la încărcarea paginii
+    const currentMovieId = movieId;
+    updateButtonText(addToQueueButton, currentMovieId, 'queue');
+    updateButtonText(addToWatchedButton, currentMovieId, 'watched');
   } catch (error) {
     console.error('Error fetching movie details:', error);
-    // Handle the error, e.g., show an error message in the modal
   }
 }
