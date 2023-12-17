9a0aa5fe59284ebaa358105ced19aac91275d8a7
Simon Khawam <khawam.simon@me.com>
2023-12-17 18:48:25 +0200

    update to library
    improving functions

--- src/js/my-library.js
+++ src/js/my-library.js
@@ -11,29 +11,6 @@ document.addEventListener('DOMContentLoaded', async function () {
   const sectionWatched = document.querySelector('.library-watched');
   const sectionQueue = document.querySelector('.library-queue');
 
-  function myLibraryClickHandler() {
-    // Ascunde sectiunea "WATCHED" și afișează sectiunea "QUEUE" la apăsarea butonului "My Library"
-    sectionWatched.style.display = 'none';
-    sectionQueue.style.display = '';
-  }
-
-  function watchedClickHandler() {
-    // Afisează sectiunea "WATCHED" și ascunde sectiunea "QUEUE" la apăsarea butonului "WATCHED"
-    sectionWatched.style.display = '';
-    sectionQueue.style.display = 'none';
-  }
-
-  function queueClickHandler() {
-    // Ascunde sectiunea "WATCHED" și afișează sectiunea "QUEUE" la apăsarea butonului "QUEUE"
-    sectionWatched.style.display = 'none';
-    sectionQueue.style.display = '';
-  }
-
-  // Adăugare event listener-uri pentru fiecare buton
-  btnMyLibrary.addEventListener('click', myLibraryClickHandler);
-  btnWatched.addEventListener('click', watchedClickHandler);
-  btnQueue.addEventListener('click', queueClickHandler);
-
   // Ascunde initial ambele sectiuni
   sectionWatched.style.display = 'none';
   sectionQueue.style.display = 'none';
@@ -42,49 +19,70 @@ document.addEventListener('DOMContentLoaded', async function () {
     // Afiseaza initial sectiunea "QUEUE" la apasarea butonului "My Library"
     sectionWatched.style.display = 'none';
     sectionQueue.style.display = '';
+
+    // Apelează funcția pentru a afișa filmele din "watched" și "queue"
+    displayMoviesFromLocalStorage('watched', sectionWatched);
+    displayMoviesFromLocalStorage('queue', sectionQueue);
   });
 
   btnWatched.addEventListener('click', function () {
     // Afiseaza sectiunea "WATCHED" la apasarea butonului "WATCHED"
     sectionWatched.style.display = '';
     sectionQueue.style.display = 'none';
+
+    // Apelează funcția pentru a afișa filmele din "watched"
+    displayMoviesFromLocalStorage('watched', sectionWatched);
   });
 
   btnQueue.addEventListener('click', function () {
     // Afiseaza sectiunea "QUEUE" la apasarea butonului "QUEUE"
     sectionWatched.style.display = 'none';
     sectionQueue.style.display = '';
+
+    // Apelează funcția pentru a afișa filmele din "queue"
+    displayMoviesFromLocalStorage('queue', sectionQueue);
   });
 
-  // Obține genurile pentru filme o singură dată
-  const genres = await getMovieGenres();
-  console.log(genres);
-
-  // Funcția pentru atașarea event listener-ului la un card de film:
   function attachCardClickListener(movieCard, movieId) {
     movieCard.addEventListener('click', () => {
       openModal(movieId); // Deschide fereastra modală când este apăsat un card de film.
     });
   }
 
-  async function displayMoviesFromLocalStorage(listType) {
+  // Funcția pentru afișarea filmelor din "watched" și "queue"
+  async function displayMoviesFromLocalStorage(listType, targetSection) {
     const movies = JSON.parse(localStorage.getItem(listType)) || [];
-    const targetSection =
-      listType === 'watched' ? sectionWatched : sectionQueue;
 
     // Golește secțiunea pentru a evita duplicarea filmelor
-    targetSection.innerHTML = '';
+    targetSection.innerHTML = ' ';
 
+    // Verifică dacă lista de filme este goală
+    if (movies.length === 0) {
+      const messageElement = document.createElement('p');
+      messageElement.textContent = `Your ${listType} list is empty.`;
+      targetSection.appendChild(messageElement);
+      return;
+    }
+
+    // Folosește Promise.all pentru a aștepta completarea tuturor cererilor asincrone
     await Promise.all(
       movies.map(async movieId => {
         try {
           const movieDetails = await getMovieDetails(movieId);
+          const genres = await getMovieGenres();
+
           // Creează un card pentru fiecare film și adaugă-l în secțiunea corespunzătoare
           const movieCard = createMovieCard(movieDetails, genres);
           attachCardClickListener(movieCard, movieId);
+
+          console.log(movieCard);
+
           targetSection.appendChild(movieCard);
         } catch (error) {
-          console.error('Error fetching movie details:', error);
+          console.error(
+            `Error fetching movie details for ID ${movieId}:`,
+            error
+          );
         }
       })
     );
@@ -112,32 +110,17 @@ document.addEventListener('DOMContentLoaded', async function () {
 
   // Adaugă un eveniment de ascultare pentru butoanele "Clear"
   clearWatchedBtn.addEventListener('click', function () {
-    clearLocalStorage('watched');
+    clearLocalStorage('watched', sectionWatched);
   });
 
   clearQueueBtn.addEventListener('click', function () {
-    clearLocalStorage('queue');
+    clearLocalStorage('queue', sectionQueue);
   });
 
-  // Funcția pentru a curăța datele din local storage și emite un eveniment personalizat
-  function clearLocalStorage(listType) {
+  // Funcția pentru a curăța datele din local storage
+  function clearLocalStorage(listType, targetSection) {
     localStorage.removeItem(listType);
-    // Emiterea unui eveniment personalizat pentru a notifica actualizarea
-    const updateEvent = new CustomEvent('localStorageUpdated', {
-      detail: { listType },
-    });
-    window.dispatchEvent(updateEvent);
+    // Actualizează afișajul pentru "watched" sau "queue"
+    displayMoviesFromLocalStorage(listType, targetSection);
   }
-
-  // Adaugă un ascultător pentru evenimentul personalizat
-  window.addEventListener('localStorageUpdated', function (event) {
-    // Actualizarea afișajului în funcție de tipul de listă
-    displayMoviesFromLocalStorage(event.detail.listType);
-  });
-  window.addEventListener('beforeunload', function () {
-    // Elimină event listener-urile aici
-    btnMyLibrary.removeEventListener('click', myLibraryClickHandler);
-    btnWatched.removeEventListener('click', watchedClickHandler);
-    btnQueue.removeEventListener('click', queueClickHandler);
-  });
 });
